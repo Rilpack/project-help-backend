fa24cffd9951e3e332b60945316d910c08885171
Rilpack <crays01234@gmail.com>
2024-07-25 10:27:41 +0700

    create post model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ("Base", "User", "Organization", "db_helper", "DatabaseHelper")
+__all__ = ("Base", "User", "Post", "Organization", "db_helper", "DatabaseHelper")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .organization import Organization
 from .user import User
+from .post import Post
