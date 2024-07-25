05b700ec6836023fda371d1fe2ac92e1f1840ab8
Rilpack <crays01234@gmail.com>
2024-07-25 10:21:19 +0700

    create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ("Base", "Organization", "db_helper", "DatabaseHelper")
+__all__ = ("Base", "User", "Organization", "db_helper", "DatabaseHelper")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .organization import Organization
+from .user import User
