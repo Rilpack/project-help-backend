14224d81b7448f5c370b095cb5704c99d62aab08
Rilpack <crays01234@gmail.com>
2024-08-19 14:23:17 +0700

    refactor config.py

--- app/core/config.py
+++ app/core/config.py
@@ -15,10 +15,7 @@ class AuthJWT(BaseModel):
 
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-
     db_url: str = "postgresql://postgres:2341@localhost:5432/ProjectHelp"
-    db_url_echo: bool = True
-
     auth_jwt: AuthJWT = AuthJWT()
 
 
