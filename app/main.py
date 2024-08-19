84d5126548526fec063120950bc31d75954178c8
Rilpack <crays01234@gmail.com>
2024-08-19 11:54:09 +0700

    refactor main.py

--- app/main.py
+++ app/main.py
@@ -9,9 +9,11 @@ from .api_v1 import router as router_v1
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
 @app.get("/")
 def get_root():
     return {"Hello": "World"}
+
+
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
