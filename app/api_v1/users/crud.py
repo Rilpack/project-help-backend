f05fb8dfb3826a5fabcf950abf8c9ce2bc2edebf
Rilpack <crays01234@gmail.com>
2024-08-20 13:34:32 +0700

    remove endpoint get user

--- app/api_v1/users/crud.py
+++ app/api_v1/users/crud.py
@@ -5,10 +5,6 @@ from app.api_v1.auth import utils as auth_utils
 from app.api_v1.users import schemas
 
 
-def get_user_by_id(db: Session, user_id: int):
-    return db.query(models.Users).filter(models.Users.id == user_id).first()
-
-
 def create_user(db: Session, user_data: schemas.UserBase):
     hashed_password = auth_utils.hash_password(user_data.password)
     user = models.Users(
--- app/api_v1/users/views.py
+++ app/api_v1/users/views.py
@@ -19,13 +19,3 @@ async def auth_user_check_self_info(
 ):
     iat = payload.get("iat")
     return user
-
-
-@router.get("/{user_id}", response_model=schemas.UserBaseView)
-async def get_user(db: db_dependency, user_id: int):
-    result = crud.get_user_by_id(db, user_id)
-    if not result:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="user is not found"
-        )
-    return result
